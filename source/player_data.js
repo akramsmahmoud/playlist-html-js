3f6f934e4290a2926bd33e50d714c70fb17b6fc8
akram mahmoud <akrammahmoud.web@gmail.com>
2022-11-12 23:06:36 +0100

    go

---
+++ source/player_data.js
@@ -0,0 +1,32 @@
+const player_data_source = [
+    {
+        season_id: 0,
+        series_id:0,
+        series_url: 'https://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)',
+        series_title: "Title season 1 series 1"
+    },
+    {
+        season_id: 0,
+        series_id: 1,
+        series_url: 'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8',
+        series_title: "Title season 1 series 2"
+    },
+    {
+        season_id: 0,
+        series_id: 2,
+        series_url: './media/S1-3.mp4',
+        series_title: "Title season 1 series 3"
+    },
+    {
+        season_id: 1,
+        series_id: 0,
+        series_url: 'https://devstreaming-cdn.apple.com/videos/streaming/examples/img_bipbop_adv_example_fmp4/master.m3u8',
+        series_title: "Title season 2 series 1"
+    },
+    {
+        season_id: 2,
+        series_id: 0,
+        series_url: 'https://amssamples.streaming.mediaservices.windows.net/634cd01c-6822-4630-8444-8dd6279f94c6/CaminandesLlamaDrama4K.ism/manifest(format=m3u8-aapl)',
+        series_title: "Title season 3 series 1"
+    },
+];
---
+++ source/playlist_data.js
@@ -0,0 +1,90 @@
+const playlist_data_source = [
+    {
+        "season_id": 0,
+        "season_name": "Season 1",
+        "season_videos": [
+            {
+                "video_id": 0,
+                "video_name": "title Season 1 series 1",
+                "video_description": `April 5, 2022 - Attack on Titan (Japanese: 進撃の巨人, Hepburn: Shingeki no Kyojin, lit. "The Advancing Giants") is a Japanese manga series written and illustrated by Hajime Isayama. It is set in a world where humanity is forced to live in cities`,
+                "video_duration": "2:00 min",
+                "video_poster": "./media/S1-1.png",
+                "video_url": "./media/S1-1.mp4",
+                
+            },
+            {
+                "video_id": 1,
+                "video_name": "title Season 1 series 2",
+                "video_description": "Lorem Ipsum data text long one 2",
+                "video_duration": "3:00 min",
+                "video_poster": "./media/S1-2.png",
+                "video_url": "./media/S1-2.mp4",
+                
+            },
+            {
+                "video_id": 2,
+                "video_name": "title Season 1 series 3",
+                "video_description": "Lorem Ipsum data text long one 3",
+                "video_duration": "4:00 min",
+                "video_poster": "./media/S1-3.png",
+                "video_url": "./media/S1-3.mp4",
+            },
+            {
+                "video_id": 3,
+                "video_name": "title Season 1 series 1",
+                "video_description": "Lorem Ipsum data text long one",
+                "video_duration": "2:00 min",
+                "video_poster": "./media/S1-3.png",
+                "video_url": "./media/S2-2.mp4",
+
+            },
+            {
+                "video_id": 4,
+                "video_name": "title Season 1 series 1",
+                "video_description": "Lorem Ipsum data text long one",
+                "video_duration": "2:00 min",
+                "video_poster": "./media/S1-3.png",
+                "video_url": "./media/S2-2.mp4",
+
+            },
+            {
+                "video_id": 5,
+                "video_name": "title Season 1 series 1",
+                "video_description": "Lorem Ipsum data text long one",
+                "video_duration": "2:00 min",
+                "video_poster": "./media/S1-3.png",
+                "video_url": "./media/S2-2.mp4",
+
+            }
+        ]
+    },
+    {
+        "season_id": 1,
+        "season_name": "Season 2",
+        "season_videos": [
+            {
+                "video_id": 0,
+                "video_name": "title Season 1 series 1",
+                "video_description": "Lorem Ipsum data text long one",
+                "video_duration": "2:00 min",
+                "video_poster": "./media/S1-3.png",
+                "video_url": "./media/S2-2.mp4",
+                
+            },
+        ]
+    },
+    {
+        "season_id": 2,
+        "season_name": "Season 3",
+        "season_videos": [
+            {
+                "video_id": 0,
+                "video_name": "title Season 1 series 1",
+                "video_description": "Lorem Ipsum data text long one",
+                "video_duration": "2:00 min",
+                "video_poster": "./media/S1-3.png",
+                "video_url": "./media/S2-3.mp4",
+            }
+        ]
+    }
+]
